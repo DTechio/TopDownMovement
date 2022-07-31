45a38a04f70fdf559f942f946416e1c36a05b80a
Daniel Techio <58491811+DTechio@users.noreply.github.com>
2022-07-31 18:03:42 -0300

    Add files via upload

---
+++ Movement.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 8f;
+
+    public Rigidbody2D rb;
+
+    float inputHorizontal;
+    float inputVertical;
+
+    void Update() //inputs
+    {
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        inputVertical = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate() //movements
+    {
+        rb.velocity = new Vector2(inputHorizontal, inputVertical).normalized * moveSpeed;
+    }
+}
